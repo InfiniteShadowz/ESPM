3252035df6ee8734a4e5cdb1b7e28eb2ce5b92ed
Fernanda Shinoda <63424056+InfiniteShadowz@users.noreply.github.com>
2020-05-18 20:16:40 -0300

    Create exercicio1.java

---
+++ semestre1/Algoritmos/Java/listaEx2/exercicio1.java
@@ -0,0 +1,29 @@
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		
+		int A = 6, B = 3, C = 2;
+		boolean res;
+		
+		// a)
+		res = (A + B / C) < (C / A + 4);
+		System.out.println("a) "+res);
+		// b)
+		res = ((B%C)+3) >= (B%A)+(C%B);
+		System.out.println("b) "+res);
+		// c)
+		res = ((B%C)+3) <= (B%A)+(C%B);
+		System.out.println("c) "+res);
+		// d)
+		res = A*B == A+B-3+C/4;
+		System.out.println("d) "+res);
+		// e)
+		res = A/B%B%A%C != B%A%B%C%A;
+		System.out.println("e) "+res);
+		
+		
+		
+		
+	}
+
+}
