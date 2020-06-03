4076019df6a7788426a47fdb2864d183522cc0ff
Fernanda Shinoda <63424056+InfiniteShadowz@users.noreply.github.com>
2020-06-03 15:27:50 -0300

    menos eficiente

---
+++ semestre1/Algoritmos/Java/primos/primos1.java
@@ -0,0 +1,43 @@
+// java
+
+package primos;
+
+import java.util.Scanner;
+
+public class primos1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n; //qnt de loops
+		int valor; // numero que sera testado
+		int qtd;
+		long inicio, fim; // variaveis para armazenar inicio e fim do tempo de processamento
+		
+		
+		System.out.println("Informe a quantidade de testes: ");
+		n = input.nextInt();
+		
+		inicio = System.currentTimeMillis();
+		
+		for(int i= 1; i <= n; i++) {
+			System.out.print("informe o valor de teste: ");
+			valor = input.nextInt();
+			qtd = 0;
+			for (int j = 1; j <= valor/2; j++) {
+				if(valor % j == 0) {
+					qtd++;
+				}
+				
+			}
+			System.out.println(qtd == 1 ?"Primo": "Não Primo");
+		}
+		
+		fim = System.currentTimeMillis();
+		System.out.println(fim-inicio);
+		
+		input.close();
+
+	}
+
+}
